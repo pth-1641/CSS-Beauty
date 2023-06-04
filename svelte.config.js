a287389de6726368e77bd1137095ab682144cd38
pth-1641 <pth16042001@gmail.com>
2023-06-04 22:53:04 +0700

    Revert to vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({ strict: false }),
+    adapter: adapter(),
   },
   preprocess: vitePreprocess(),
 };
